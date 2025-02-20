f8d88f5fcb035500b2e962e1702c0eb6e6f7fc0b
Daniel M <85372728+Overclocker2077@users.noreply.github.com>
2025-02-20 17:43:26 -0500

    Update ObstacleCourse.java

--- EvoNeurolNet/ObstacleCourse.java
+++ EvoNeurolNet/ObstacleCourse.java
@@ -1,99 +1,96 @@
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.*;
+import java.awt.Graphics;
 
 
 public class ObstacleCourse extends JPanel {
     private Timer t;
 
-    Wall wall;          // obstacle
-    int num_of_agents = 10;
+    Wall[] walls;          // obstacle
+    int num_of_agents = 100;
     Agent[] agents = new Agent[num_of_agents];           // AI players
     Agent most_fit;           // keep track of most fit agent
     Agent prev_most_fit;       
-    //window dimensions
+    int goalx = 950;
+    int goaly = 350;
+    double[][][] most_fit_hidden_layers;    
+    double[][] most_fit_output_layer;
+
     int win_width;      
     int win_height;
     int startx = 100;
     int starty = 400;
-    int dead_count = 0;
+    int timer = 0;
     int generation_count = 0;
+    int touching = 0;
     int[] time = {0,0}; // survival time of current most fit and previous most fit
     
     public ObstacleCourse(int w, int h) {
         win_height = h;
         win_width = w;
-        wall = new Wall(w, h-500, 600, 40);
         
+        // walls = new Wall[] {new Wall(150, win_height-600, 400, 15)};
         // make 10 Agents for first generation 
         for (int i = 0; i < num_of_agents; i++)
-            agents[i] = new Agent(startx + (15 * i), starty, 10,10);
+            agents[i] = new Agent(startx, starty, 10,10);
         prev_most_fit = agents[0];
-        t = new Timer(50, new Listener());	   // 0 delay between frames
+        most_fit = agents[0];
+        t = new Timer(0, new Listener());	   // 0 delay between frames
         t.start(); 
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        wall.draw_wall(g);
+        // for (Wall wall: walls)
+        //     wall.draw(g);
+
         g.drawString("Gen: " + generation_count, 10, 10);
+        g.drawRect(goalx, goaly, 10, 10);
         // draw all agents
         for (Agent agent : agents) {
-            if (!agent.dead)
-                agent.draw(g);
+            agent.draw(g);
         }
     }
 
     private class Listener implements ActionListener {
        public void actionPerformed(ActionEvent e)	//this is called for each timer iteration
-       { 
-        // move wall to the left 
-        wall.x -= 20;
-        
-        // Give agents data on it's environment, so it can make decisions
-        for (int i = 0; i < agents.length; i++) {
-            agents[i].velocity += 4;  // gravity 
-            agents[i].update();
-            if (!agents[i].dead) {
-                agents[i].perform_action(wall.height);  // give agent the wall height, so it could maneuver above it
-                // check if agent dies
-                if ((wall.collides(agents[i].x, agents[i].y) || agents[i].y > win_height || agents[i].y < 0)) {
-                    agents[i].dead = true;
-                    dead_count++;
-                } 
-                else 
-                    most_fit = agents[i];   // eventually the last surviving will be stored in most_fit
-            }
+       {
+        for (int i = 0; i < num_of_agents; i++) {
+            // for (Wall wall: walls) {
+            //     if (wall.collides(agents[i].x, agents[i].y))
+            //         touching = 1;
+            // }
+            agents[i].perform_action(new double[] {goalx, goaly, agents[i].x, agents[i].y, touching});
+            // touching = 0;
         }
-        // if all agents are dead; get most_fit and start a new generation
-        if (dead_count == num_of_agents) {
-            if (time[1] < time[0]) 
+        if (timer > 180) {
+            for (int i = 0; i < num_of_agents; i++) {
+                //System.out.println(agents[i].fitness(goalx, goaly));
+                // fitness returns distance from the goal, so value = better fitness
+                if (agents[i].fitness(goalx, goaly) < most_fit.fitness(goalx, goaly))
+                    most_fit = agents[i];
+            }
+            if (prev_most_fit.fitness(goalx, goaly) < most_fit.fitness(goalx, goaly)) 
                 most_fit = prev_most_fit;
-            else {
-                time[0] = time[1];
-                prev_most_fit = most_fit;
-            }
-            time[1] = 0;
-            
-            double[][][] hl = most_fit.brain.hidden_layers;
-            double[][] ol = most_fit.brain.output_layer;
-            for (int i = 0; i < agents.length; i++) {
-                agents[i].new_brain(hl, ol);
-                agents[i].x = startx + (15 * i);
+            prev_most_fit = most_fit;
+            // update population with most_fit
+            most_fit_hidden_layers = most_fit.hl_copy(most_fit.brain.hidden_layers);  
+            most_fit_output_layer = most_fit.deepCopy(most_fit.brain.output_layer);    
+            for (int i = 0; i < num_of_agents; i++) {
+                agents[i].new_brain(most_fit_hidden_layers, most_fit_output_layer);
+                agents[i].x = startx;
                 agents[i].y = starty;
-                agents[i].dead = false;
             }
-            dead_count = 0;
-            wall.x = win_width;
+            timer = 0;
             generation_count++;
         }
-        if (wall.x < 0) {
-            wall.x = win_width;
-            wall.height = (int)(Math.random() * 800);
-            wall.y = win_height - wall.height;
+        if (generation_count % 50 == 0) {
+            goaly = (int)(Math.random() * 500);
+            goalx = (int)(Math.random() * 200) + 500;
         }
-        time[1]++;
+        //System.out.println(timer);
+        timer++;
         repaint();
        }
     }
